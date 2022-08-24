8173e58bcf692dd1de84c44f980dffbc3bcfdeb1
Ashukla011 <shuklavashu9696@gmail.com>
2022-08-24 22:40:39 +0530

    Avnish

---
+++ src/scripts/A_Event.js
@@ -0,0 +1,25 @@
+
+
+window.addEventListener("load",function(){
+    start ()
+})
+
+let i=0;
+function start(){
+    let arr=["FREE SHIPPING FROM BLUEWARDS MEMBERS","FREE GIFTS WITH PURCHASE BROWSE NOW", "FREE SAMPLES WITH ALL ORDERS"]
+     let Acon=document.getElementById("Aslider")
+
+     setInterval(()=>{
+        if(i===arr.length){
+            i=0;
+        }
+        Acon.innerHTML=null;
+        let fist_heading=arr[i]
+
+        let p=document.createElement("p")
+        p.innerText=fist_heading;
+        Acon.append(p)
+        i++
+     },2000)
+
+}

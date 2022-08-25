51ac1598891e6500b0573b6e5b7f70238693eb29
Shivani Pandey <104144826+shivanipandey22@users.noreply.github.com>
2022-08-25 22:23:11 +0530

    shivani

--- src/scripts/productPage.js
+++ src/scripts/productPage.js
@@ -1,4 +1,4 @@
-import {sidebar,sortby} from "./components/sidebar.js";
+import {sidebar,sortby} from "../components/sidebar.js";
 
 
 
@@ -6,12 +6,23 @@ document.getElementById("sidebar").innerHTML=sidebar();
 document.getElementById("right_main_select").innerHTML=sortby();
 
 var flag = true;
-  document.querySelector("#dress1").addEventListener("click", function () {
+  document.querySelector("#fil_one").addEventListener("click", function () {
     if (flag) {
       document.querySelector(".hide").style.display = "none";
       return (flag = false);
     } else {
       document.querySelector(".hide").style.display = "block";
       return (flag = true);
+    }
+  });
+
+  var flag1 = true;
+  document.querySelector("#fil_two").addEventListener("click", function () {
+    if (flag1) {
+      document.querySelector(".hide2").style.display = "none";
+      return (flag1 = false);
+    } else {
+      document.querySelector(".hide2").style.display = "block";
+      return (flag1 = true);
     }
   });

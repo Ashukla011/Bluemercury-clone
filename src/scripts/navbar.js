9b5dcd81da1a5f7090b4042f0c800b7dcd14e364
raghavendra7992 <103146867+raghavendra7992@users.noreply.github.com>
2022-08-25 23:54:24 +0530

    raghav

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -15,6 +15,26 @@
    searcheng.type="search"
    cont.append(searcheng)
   }
+  const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const offcanvasMenu = document.querySelector('.offcanvas-menu')
+
+openBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    offcanvasMenu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    offcanvasMenu.classList.remove('active');
+}); 
+let contra=document.getElementById("container4")
+contra.innerHTML=null;
+let h=document.createElement("h3")
+h.innerText="Product"
+let h1=document.createElement("h3")
+h1.innerText="Add To cart"
+contra.append(h,h1)
   let rappend=document.getElementById("search1");
   rappend.onclick=()=>{
    getData1()
@@ -27,21 +47,17 @@
    appendr(data)
   }
   let appendr=(data)=>{
-   let cont=docume.getElementById("container1")
+   let cont=document.getElementById("container1")
    cont.innerHTML=null;
    data.forEach(()=>{
       let h=document.createElement("div");
       let h2=document.createElement("img");
       h2.setAttribute("class","img1");
-      let h1
+      
 
    })
   }
 let nav=document.getElementsByClassName("heading_one")
 nav.onclick=()=>{
    navopen()
-}
-let navopen=()=>{
-  
-   
 }

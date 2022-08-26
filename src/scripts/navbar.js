454c4eada22386b045173f02d9ac451bbf8beb95
raghavendra7992 <103146867+raghavendra7992@users.noreply.github.com>
2022-08-26 23:37:19 +0530

    raghav

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,5 +1,5 @@
 
-   import { header,footer } from "../raghav component/navbar.js";
+   import { header,footer } from "../components/navbar.js";
    document.getElementById("head").innerHTML=header();
    document.getElementById("foot").innerHTML=footer();
    let search=document.getElementById("rsearch")
@@ -9,12 +9,21 @@
   let  rgetData=()=>{
    let cont=document.getElementById("rcontainer")
    cont.innerHTML=null;
+   let div=document.createElement("div")
    let searcheng=document.createElement("input");
    searcheng.setAttribute("id","search1");
    searcheng.placeholder="SEARCH"
+   searcheng.onkeyup=()=>{
+      getData1()
+   }
+   
    searcheng.type="search"
-   cont.append(searcheng)
+   div.append(searcheng)
+   cont.append(div)
   }
+
+// side nav start
+
   const openBtn = document.querySelector('.open-btn');
 const closeBtn = document.querySelector('.close-btn');
 const offcanvasMenu = document.querySelector('.offcanvas-menu')
@@ -31,33 +40,77 @@ closeBtn.addEventListener('click', function (e) {
 let contra=document.getElementById("container4")
 contra.innerHTML=null;
 let h=document.createElement("h3")
-h.innerText="Product"
-let h1=document.createElement("h3")
-h1.innerText="Add To cart"
-contra.append(h,h1)
-  let rappend=document.getElementById("search1");
-  rappend.onclick=()=>{
-   getData1()
-  }
-  let getData1=async()=>{
-   let query=document.getElementById("search1").value;
-   let res=await fetch("");
-   let data=res.json();
+h.innerText="Your Bag"
+h.setAttribute("id","yourbag")
+let h1=document.createElement("img")
+h1.src="https://cdn0.iconfinder.com/data/icons/interface-line-4/48/delete_close_cancel-256.png"
+h1.setAttribute("id","delete")
+contra.append(h,h1);
+let del=document.getElementById("delete")
+del.onclick=()=>{
+   delete1()
+}
+let delete1=()=>{
+   offcanvasMenu.classList.remove('active');
+}
+
+// side nav end
+let id=document.getElementById("search1");
+  let getData1=async(id)=>{
+   // let query=document.getElementById("search1").value;
+   let res=await fetch (`https://still-chamber-16033.herokuapp.com/makeup_data?name=${id}`);
+   let data=await res.json();
    console.log(data)
-   appendr(data)
   }
+  getData1()
+  
   let appendr=(data)=>{
-   let cont=document.getElementById("container1")
+   let cont=docume.getElementById("container1")
    cont.innerHTML=null;
    data.forEach(()=>{
       let h=document.createElement("div");
       let h2=document.createElement("img");
       h2.setAttribute("class","img1");
-      
+      let h1
 
    })
   }
-let nav=document.getElementsByClassName("heading_one")
-nav.onclick=()=>{
-   navopen()
+let data=async()=>{
+   
+   let res=await fetch(`https://still-chamber-16033.herokuapp.com/makeup_data`);
+  
+   let data=await res.json()
+   console.log(data)
+}
+data();
+let jhola1=()=>{
+let jhol=JSON.parse(localStorage.getItem("jhola"))||[];
+let cont=document.createElement("container4")
+cont.innerHTML=null;
+let hdiv=document.createElement("div");
+let h1=document.createElement("img")
+let h2=document.createElement("p")
+let h3=document.createElement("h5");
+let h4=document.createElement("h6");
+let hdiv1=document.createElement("div")
+let h5=document.createElement("img")
+let h6=document.createElement("h5");
+let h7=document.createElement("button")
+hdiv.innerText="FREE RETURNS IN ALL ORDERS"
+}
+jhola1();
+
+let email1=document.getElementById("email")
+email1.onclick=()=>{
+   mailSent()
+}
+function mailSent() {
+   let email = document.getElementById("email").value;
+
+   if (email != null) {
+       setTimeout(function () {
+           alert("Subscribed, you will receive daily tips on mail!");
+           document.getElementById("email").value = null;
+       }, 4000);
+   }
 }
